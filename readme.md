8b6f9f8c3ac437dc743857c2b71e28f1c74a7fa7
nathanq <nathanstefanqian@gmail.com>
2024-08-16 21:55:30 +0800

    first commit

---
+++ readme.md
@@ -0,0 +1,4 @@
+# startup
+在终端运行
+npm install
+npm run dev:mp-weixin
---
+++ src/pages/home/index.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <button class="btn" @click="handleBtn">点我获取code</button>
+  </div>
+</template>
+
+<script setup lang="ts">
+const handleBtn = () => {
+  uni.login({
+    success: res => {
+      console.log(res)
+    }
+  })
+}
+</script>
+<style scoped>
+
+</style>
