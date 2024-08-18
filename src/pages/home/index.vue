3ebd66db8ad60ec0aa43db65260ff947d53def0f
nathanq <nathanstefanqian@gmail.com>
2024-08-18 13:24:10 +0800

    订阅消息

--- src/pages/home/index.vue
+++ src/pages/home/index.vue
@@ -1,6 +1,7 @@
 <template>
   <div>
     <button class="btn" @click="handleBtn">点我获取code</button>
+    <button class="btn" @click="handleSub">一次性订阅消息</button>
   </div>
 </template>
 
@@ -8,6 +9,15 @@
 const handleBtn = () => {
   uni.login({
     success: res => {
+      console.log(res)
+    }
+  })
+}
+
+const handleSub = () => {
+  uni.requestSubscribeMessage({
+    tmplIds: ['Gp5FiHAZ8ym599r7gRFP9EYDKVhYg9nC20T0wXwQISA'],
+    success (res) {
       console.log(res)
     }
   })
