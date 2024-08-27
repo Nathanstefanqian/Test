101e12e54a2535f4bdf70122441aa7d5d617df28
nathanq <nathanstefanqian@gmail.com>
2024-08-27 11:34:08 +0800

    手机号验证

--- src/pages/home/index.vue
+++ src/pages/home/index.vue
@@ -2,6 +2,7 @@
   <div>
     <button class="btn" @click="handleBtn">点我获取code</button>
     <button class="btn" @click="handleSub">一次性订阅消息</button>
+    <button open-type="getPhoneNumber" @getphonenumber="handlePhone">手机号快速验证</button>
   </div>
 </template>
 
@@ -21,6 +22,12 @@ const handleSub = () => {
       console.log(res)
     }
   })
+}
+
+const handlePhone = (e: any) => {
+    console.log('code', e.detail.code)  // 动态令牌
+    console.log('msg', e.detail.errMsg) // 回调信息（成功失败都会返回）
+    console.log('error', e.detail.errno)  // 错误码（失败时返回）
 }
 </script>
 <style scoped>
